b953ddafeb4b0bac8fc22ca6ca3526828db2f61d
rafael rotiroti <rotirotirafa@gmail.com>
2023-07-01 00:00:59 -0300

    feat: players CRUD

--- src/schemas/players/__init__.py
+++ src/schemas/players/__init__.py
@@ -6,8 +6,8 @@ from pydantic import BaseModel
 class PlayerSchema(BaseModel):
     player_id: Optional[int]
     nickname: str
-    status: str
-    image_url: str
+    status: Optional[str]
+    image_url: Optional[str]
     profile_desc: str
     main_position: str
     secondary_position: str
@@ -16,7 +16,41 @@ class PlayerSchema(BaseModel):
     updated_date: datetime = None
     user_id: int
     team_id: Optional[int]
-    goals_id: Optional[int]
+
+    class Config:
+        orm_mode = True
+
+
+class PlayerSchemaInsertResponse(BaseModel):
+    player_id: Optional[int]
+    nickname: str
+    status: Optional[str]
+    image_url: Optional[str]
+    profile_desc: str
+    main_position: str
+    secondary_position: str
+    signed: bool
+    created_date: datetime = None
+    updated_date: datetime = None
+    user_id: int
+    team_id: Optional[int]
+
+    class Config:
+        orm_mode = True
+
+
+class PlayerUpdateRequest(BaseModel):
+    player_id: Optional[int]
+    nickname: Optional[str]
+    status: Optional[str]
+    image_url: Optional[str]
+    profile_desc: Optional[str]
+    main_position: Optional[str]
+    secondary_position: Optional[str]
+    signed: Optional[bool]
+    updated_date: datetime = None
+    user_id: int
+    team_id: Optional[int]
 
     class Config:
         orm_mode = True
